eb438603c5513ef8bbf2f8e1f3396fa717aff62a
Greco <Greco@Greco>
2017-09-13 12:37:04 -0300

    Angular JS - Ajax con $http segunda parte

---
+++ web/js/ajaxcon$http.js
@@ -0,0 +1,26 @@
+angular.module("MiSegundaApp", [])
+        .controller("SegundoControlador", function ($scope, $http) {
+            $scope.posts = [];
+            $scope.newPost = {};
+            $http.get("http://jsonplaceholder.typicode.com/posts")
+                    .success(function (data) {
+                        console.log(data);
+                        $scope.posts = data;
+                    })
+                    .error(function (err) {
+                    });
+            $scope.addPost = function () {
+                $http.post("http://jsonplaceholder.typicode.com/posts", {
+                    title: $scope.newPost.title,
+                    body: $scope.newPost.body,
+                    userId: 1
+                })
+                        .success(function (data, status, headers, config) {
+                            $scope.posts.push(data);
+                            $scope.newPost = {};
+                        })
+                        .error(function (error, status, headers, config) {
+                            console.log(error);
+                        });
+            };
+        });    
